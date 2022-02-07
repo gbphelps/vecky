cf741b197ecbd07c15ce5eca7e92ead912a2031e
Grant Phelps <gbphelps2@gmail.com>
2022-02-07 17:03:59 -0500

    password hashing

--- src/server/src/api/users/index.ts
+++ src/server/src/api/users/index.ts
@@ -1,10 +1,23 @@
 import express from 'express';
+import bcrypt from 'bcrypt';
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  console.log(req.body.username);
-  res.send();
+router.post('/', async (req, res) => {
+  const { body: { username, password } } = req;
+
+  const user = await req.db.collection('users').findOne({ username });
+
+  if (user) {
+    res.status(409).send('username already taken');
+    return;
+  }
+
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  req.db.collection('users').insertOne({ username, passwordHash });
+
+  res.status(200).send();
 });
 
 export default router;
