938b852081517df0ef02b05b7deaebacc6e0c49f
Grant Phelps <gbphelps2@gmail.com>
2022-02-24 11:04:36 -0500

    dual approach

--- src/frontend/src/canvas/utils/intersections.ts
+++ src/frontend/src/canvas/utils/intersections.ts
@@ -1,48 +1,110 @@
 import {
   bezierOfDegree,
-  // bbox,
-  // getOverlap,
-  // split2d,
+  bbox,
+  getOverlap,
+  split2d,
 } from './bezier';
 import Vec2 from './vec2';
 import { getAbstractCubicRoots } from './abstractRoots';
 import { findRoots } from './roots';
 
-// function intersections(a: Vec2[], b: Vec2[], iterations: number): Vec2[] {
-//   // eslint-disable-next-line no-param-reassign
-//   if (iterations == null) iterations = 20;
+/*
+  generate back-of-napkin ranges for the intersections.
 
-//   let res: Vec2[] = [];
+  Approach is to calculate bboxes if curve a and b. If
+  they overlap, split both curves in half. For each of
+  the four resulting combinations of curve pieces,
+  repeat. Do this for a given number of iterations
+  or bail early if the bounding boxes are small enough
+  to indicate we've essentially found the answer.
 
-//   const boxA = bbox(a);
-//   const boxB = bbox(b);
+  We do this step first because the more precise equality
+  we run in `intersections()` is in t and s and can
+  miss intersections due to extreme slopes, undefined regions,
+  and asymptotes.
 
-//   const overlap = getOverlap(boxA, boxB);
-//   if (!overlap) return res;
+  This calculates intersections in x and y rather than
+  t and s, which is much more reliable for rough calculations,
+  but becomes less and less precise with each iteration
+  due to compounded error margins from the split() function.
 
-//   const delx = overlap.x[1] - overlap.x[0];
-//   const dely = overlap.y[1] - overlap.y[0];
+  This method shrinks the "regions of interest" that need
+  to be investigated with `intersections()`, while still
+  allowing us to get essentially double float precision
+  (1e-16) compared to a MAX of about 1e-12 by using
+  just this roughIntersections method.
+*/
+function roughIntersections(
+  a: Vec2[],
+  b: Vec2[],
+) {
+  return _rough(
+    a,
+    b,
+    10,
+    0,
+    0,
+    1,
+  );
+}
 
-//   if (delx ** 2 + dely ** 2 < 1e-16 ** 2 || iterations === 0) {
-//     return [new Vec2(
-//       (overlap.x[0] + overlap.x[1]) / 2,
-//       (overlap.y[0] + overlap.y[1]) / 2,
-//     )];
-//   }
+function _rough(
+  a: Vec2[],
+  b: Vec2[],
+  iterations: number,
+  ta: number,
+  tb: number,
+  size: number,
+): {
+  ta: [number, number],
+  tb: [number, number]
+}[] {
+  let res: {
+    ta: [number, number],
+    tb: [number, number]
+  }[] = [];
 
-//   const aSplit = split2d(a, 0.5);
-//   const bSplit = split2d(b, 0.5);
+  const boxA = bbox(a);
+  const boxB = bbox(b);
 
-//   aSplit.forEach((aPiece) => {
-//     bSplit.forEach((bPiece) => {
-//       res = res.concat(intersections(aPiece, bPiece, iterations - 1));
-//     });
-//   });
+  const overlap = getOverlap(boxA, boxB);
+  if (!overlap) return res;
 
-//   return res;
-// }
+  const delx = overlap.x[1] - overlap.x[0];
+  const dely = overlap.y[1] - overlap.y[0];
+
+  if (delx ** 2 + dely ** 2 < 1e-16 ** 2 || iterations === 0) {
+    return [{
+      ta: [ta, ta + size],
+      tb: [tb, tb + size],
+    }];
+  }
+
+  const aSplit = split2d(a, 0.5);
+  const bSplit = split2d(b, 0.5);
+  const newSize = size / 2;
+
+  aSplit.forEach((aPiece, i) => {
+    bSplit.forEach((bPiece, j) => {
+      res = res.concat(
+        _rough(
+          aPiece,
+          bPiece,
+          iterations - 1,
+          ta + (i * newSize),
+          tb + (j * newSize),
+          newSize,
+        ),
+      );
+    });
+  });
+
+  return res;
+}
 
 function intersections(aPoints: Vec2[], bPoints: Vec2[]): Vec2[] {
+  const checkpoints = roughIntersections(aPoints, bPoints).map(({ tb }) => tb);
+
   const [a, b] = [aPoints, bPoints].map((points, i) => {
     const bz = bezierOfDegree(points.length);
     const x = bz(...points.map((p) => p.x)).unproject(1 - i);
@@ -72,25 +134,13 @@ function intersections(aPoints: Vec2[], bPoints: Vec2[]): Vec2[] {
   let results: number[] = [];
   [root1, root2, root3].forEach((r) => {
     const solver = zero2.get1dSolver(1, { 0: r });
-    results = results.concat(findRoots({
-      fn: solver,
-      range: [0, 1],
-      numSegments: 20,
-      maxIterations: 20,
-      precision: 1e-16,
-    }));
 
-    for (let i = 0; i < imaginaryBounds.length; i++) {
-      const bound = imaginaryBounds[i];
-      const range: [number, number] = Number.isNaN(solver(bound - 0.01))
-        ? [bound, bound + 0.01]
-        : [bound - 0.01, bound];
-      // TODO probably sample more densely right beside the bound
-
+    for (let i = 0; i < checkpoints.length; i++) {
+      const range = checkpoints[i];
       results = results.concat(findRoots({
         fn: solver,
         range,
-        numSegments: 100,
+        numSegments: 10,
         maxIterations: 20,
         precision: 1e-16,
       }));
