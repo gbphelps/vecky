acf18be241c9e4bd8e058839e65293c76e523e5b
Grant Phelps <gbphelps2@gmail.com>
2022-02-23 23:15:18 -0500

    test for commonNormals

--- src/frontend/src/canvas/utils/__tests__/commonSlopes.ts
+++ src/frontend/src/canvas/utils/__tests__/commonSlopes.ts
@@ -1,22 +1,22 @@
-import { commonTangents } from '../commonSlopes';
+import { commonTangents, commonNormals } from '../commonSlopes';
 import Vec2 from '../vec2';
 
-describe('Common Tangents', () => {
-  test('Sanity check', () => {
-    const a = [
-      new Vec2(50, 0),
-      new Vec2(100, -100),
-      new Vec2(100, 100),
-      new Vec2(0, 0),
-    ];
+describe('Common Slopes', () => {
+  const a = [
+    new Vec2(50, 0),
+    new Vec2(100, -100),
+    new Vec2(100, 100),
+    new Vec2(0, 0),
+  ];
 
-    const b = [
-      new Vec2(200, 0),
-      new Vec2(100, -200),
-      new Vec2(100, 200),
-      new Vec2(200, 100),
-    ];
+  const b = [
+    new Vec2(200, 0),
+    new Vec2(100, -200),
+    new Vec2(100, 200),
+    new Vec2(200, 100),
+  ];
 
+  test('Sanity check - commonTangents', () => {
     const ct = commonTangents(a, b);
 
     const serialized = ct.map((pair) => pair.map(({ x, y }) => ({
@@ -30,5 +30,19 @@ describe('Common Tangents', () => {
       [{ x: '70.6672', y: '20.1954' }, { x: '140.2282', y: '-51.7938' }],
       [{ x: '80.5403', y: '-24.4935' }, { x: '140.8690', y: '93.2993' }],
     ]);
+  });
+
+  test('Sanity check - commonNormals', () => {
+    const ct = commonNormals(a, b);
+
+    const serialized = ct.map((pair) => pair.map(({ x, y }) => ({
+      x: x.toFixed(4),
+      y: y.toFixed(4),
+    })));
+
+    expect(serialized).toEqual([[
+      { x: '82.6810', y: '-8.6217' },
+      { x: '125.6965', y: '-5.0976' },
+    ]]);
   });
 });
