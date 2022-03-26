42b62526c579c4665bf8373bddf79dafab073604
Grant Phelps <gbphelps2@gmail.com>
2022-03-26 13:20:19 -0400

    clean up test

--- src/frontend/src/canvas/tools/__tests__/penTool.ts
+++ src/frontend/src/canvas/tools/__tests__/penTool.ts
@@ -1,8 +1,31 @@
 import { fireEvent } from '@testing-library/dom';
 import ResizeObserver from 'resize-observer-polyfill';
 import { getMouseEvent } from '../testUtils';
-
+import Vec2 from '../../utils/vec2';
 import initCanvas from '../../initCanvas';
+import { TContext } from '../../types';
+
+function drag(element: Element, from: Vec2, to: Vec2) {
+  fireEvent(element, getMouseEvent('mousemove', {
+    offsetX: from.x,
+    offsetY: from.y,
+  }));
+  fireEvent(element, getMouseEvent('mousedown', {}));
+  fireEvent(element, getMouseEvent('mousemove', {
+    offsetX: to.x,
+    offsetY: to.y,
+  }));
+  fireEvent(element, getMouseEvent('mouseup', {}));
+}
+
+function click(element: Element, at: Vec2) {
+  fireEvent(element, getMouseEvent('mousemove', {
+    offsetX: at.x,
+    offsetY: at.y,
+  }));
+  fireEvent(element, getMouseEvent('mousedown', {}));
+  fireEvent(element, getMouseEvent('mouseup', {}));
+}
 
 // Need to imperatively trigger this ro event so we can initialize the screenManager
 jest.mock('resize-observer-polyfill', () => class FakeResizeObserver {
@@ -21,111 +44,87 @@ jest.mock('resize-observer-polyfill', () => class FakeResizeObserver {
 });
 
 describe('Pen tool', () => {
+  let ctx: TContext = null as any;
+
   beforeEach(() => {
     document.body.innerHTML = '';
-  });
-
-  test('Makes one linear curve', () => {
-    const A = {
-      offsetX: 30,
-      offsetY: 40,
-    };
-    const B = {
-      offsetX: 80,
-      offsetY: 90,
-    };
-
     const root = document.createElement('div');
     document.body.appendChild(root);
-    const { ctx } = initCanvas(root);
-
-    expect(document.getElementsByTagName('path')).toHaveLength(0);
-
-    // add our first point
-    fireEvent(ctx.root, getMouseEvent('mousemove', A));
-
-    // click, don't drag
-    fireEvent(ctx.root, getMouseEvent('mousedown', {}));
-    fireEvent(ctx.root, getMouseEvent('mouseup', {}));
-
-    fireEvent(ctx.root, getMouseEvent('mousemove', B));
-
-    expect(document.getElementsByTagName('path')[0].getAttribute('d')).toEqual(
-      'M 30 40 L 80 90',
-    );
+    ctx = initCanvas(root).ctx;
   });
 
-  test('Makes one quadratic bezier curve', () => {
-    const A = {
-      offsetX: 30,
-      offsetY: 40,
-    };
-    const B = {
-      offsetX: 80,
-      offsetY: 90,
-    };
-    const C = {
-      offsetX: -30,
-      offsetY: 20,
-    };
+  describe('Drawing a single curve', () => {
+    test('Makes one linear curve', () => {
+      const A = new Vec2(30, 40);
+      const B = new Vec2(80, 90);
 
-    const root = document.createElement('div');
-    document.body.appendChild(root);
-    const { ctx } = initCanvas(root);
+      expect(document.getElementsByTagName('path')).toHaveLength(0);
 
-    // add our first point
-    fireEvent(ctx.root, getMouseEvent('mousemove', A));
-    fireEvent(ctx.root, getMouseEvent('mousedown', {}));
+      click(ctx.root, A);
+      click(ctx.root, B);
 
-    // drag to B
-    fireEvent(ctx.root, getMouseEvent('mousemove', B));
-    fireEvent.mouseUp(ctx.root);
+      const paths = document.getElementsByTagName('path');
+      expect(paths).toHaveLength(1);
+      expect(paths[0].getAttribute('d')).toEqual(
+        'M 30 40 L 80 90',
+      );
+    });
 
-    fireEvent(ctx.root, getMouseEvent('mousemove', C));
+    test('Makes one quadratic bezier curve (drag first)', () => {
+      const A = new Vec2(30, 40);
+      const B = new Vec2(80, 90);
+      const C = new Vec2(-30, 20);
 
-    expect(document.getElementsByTagName('path')[0].getAttribute('d')).toEqual(
-      'M 30 40 Q 80 90 -30 20',
-    );
+      drag(ctx.root, A, B);
+      click(ctx.root, C);
+
+      const paths = document.getElementsByTagName('path');
+
+      expect(paths).toHaveLength(1);
+      expect(paths[0].getAttribute('d')).toEqual(
+        'M 30 40 Q 80 90 -30 20',
+      );
+    });
+
+    test('Makes one quadratic bezier curve (drag second)', () => {
+      const A = new Vec2(30, 40);
+      const B = new Vec2(80, 90);
+      const C = new Vec2(-30, 20);
+
+      click(ctx.root, A);
+      drag(ctx.root, B, C);
+
+      const paths = document.getElementsByTagName('path');
+
+      expect(paths).toHaveLength(1);
+      expect(paths[0].getAttribute('d')).toEqual(
+        'M 30 40 Q 190 160 80 90',
+      );
+    });
+
+    test('Makes one cubic bezier curve', () => {
+      const A = new Vec2(0, 0);
+      const B = new Vec2(0, 100);
+      const C = new Vec2(100, 0);
+      const D = new Vec2(100, 100);
+
+      drag(ctx.root, A, B);
+      drag(ctx.root, C, D);
+
+      const paths = document.getElementsByTagName('path');
+
+      expect(paths).toHaveLength(1);
+      expect(paths[0].getAttribute('d')).toEqual(
+        'M 0 0 C 0 100 100 -100 100 0',
+      );
+    });
   });
 
-  test('Makes one cubic bezier curve', () => {
-    const A = {
-      offsetX: 0,
-      offsetY: 0,
-    };
-    const B = {
-      offsetX: 0,
-      offsetY: 100,
-    };
-    const C = {
-      offsetX: 100,
-      offsetY: 0,
-    };
-    const D = {
-      offsetX: 100,
-      offsetY: 100,
-    };
+  // describe('Joining two curves', () => {
+  //   const root = document.createElement('div');
+  //   document.body.appendChild(root);
+  //   const { ctx } = initCanvas(root);
+  // });
 
-    const root = document.createElement('div');
-    document.body.appendChild(root);
-    const { ctx } = initCanvas(root);
-
-    // add our first point
-    fireEvent(ctx.root, getMouseEvent('mousemove', A));
-    fireEvent(ctx.root, getMouseEvent('mousedown', {}));
-
-    // drag to B
-    fireEvent(ctx.root, getMouseEvent('mousemove', B));
-    fireEvent.mouseUp(ctx.root);
-
-    fireEvent(ctx.root, getMouseEvent('mousemove', C));
-
-    // drag to D
-    fireEvent(ctx.root, getMouseEvent('mousedown', {}));
-    fireEvent(ctx.root, getMouseEvent('mousemove', D));
-
-    expect(document.getElementsByTagName('path')[0].getAttribute('d')).toEqual(
-      'M 0 0 C 0 100 100 -100 100 0',
-    );
-  });
+  // describe('closing a curve', () => {});
 });
