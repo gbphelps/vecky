d8af1f1cda0164f55cb42bad5cedb67939c0d686
Grant Phelps <gbphelps2@gmail.com>
2022-06-07 10:33:14 -0400

    styles for dropdown

--- src/frontend/src/glslColorPicker/dropDown/index.ts
+++ src/frontend/src/glslColorPicker/dropDown/index.ts
@@ -7,6 +7,7 @@ class DropDown {
   button: HTMLButtonElement;
   publisher: PubSub<string>;
   menu: HTMLDivElement;
+  menuContainer: HTMLDivElement;
 
   constructor({
     root, options, publisher,
@@ -29,20 +30,101 @@ class DropDown {
       this.button.innerText = v;
     });
 
+    this.menuContainer = document.createElement('div');
+
+    this.container.appendChild(this.menuContainer);
+
     this.menu = document.createElement('div');
-    root.appendChild(this.menu);
+    this.menuContainer.appendChild(this.menu);
+
+    this.container.addEventListener('click', (e) => {
+      e.stopPropagation();
+    });
+
+    document.addEventListener('click', this.close);
+
+    Object.assign(this.menuContainer.style, {
+      display: 'block',
+      position: 'fixed',
+      zIndex: 2,
+      background: 'white',
+      border: '1px solid rgba(0,0,0,.1)',
+      borderRadius: '3px',
+      borderTop: 'none',
+      height: '0',
+      transition: 'height .1s, visibility .1s',
+      borderTopRightRadius: 0,
+      borderTopLeftRadius: 0,
+      overflow: 'hidden',
+      visibility: 'hidden',
+    });
+
+    this.button.addEventListener('click', () => {
+      const { bottom, left, width } = this.button.getBoundingClientRect();
+      const isExpanded = this.menuContainer.style.visibility === 'visible';
+
+      if (isExpanded) {
+        this.close();
+        return;
+      }
+
+      Object.assign(this.button.style, {
+        borderBottomLeftRadius: 0,
+        borderBottomRightRadius: 0,
+      });
+
+      Object.assign(this.menuContainer.style, {
+        top: `${bottom}px`,
+        left: `${left}px`,
+        width: `${width}px`,
+        height: `${this.menu.getBoundingClientRect().height}px`,
+        visibility: 'visible',
+      });
+    });
 
     options.forEach((o) => {
       const button = document.createElement('button');
       button.value = o.value;
       button.innerText = o.label;
+      Object.assign(button.style, {
+        padding: '8px',
+        border: 'none',
+        outline: 'none',
+        background: 'none',
+        display: 'block',
+        width: '100%',
+        textAlign: 'left',
+      });
+
+      this.publisher.subscribe((opt) => {
+        Object.assign(button.style, {
+          background: opt === o.label ? '#fafaff' : 'white',
+          color: opt === o.label ? 'dodgerblue' : 'black',
+        });
+      });
 
       button.addEventListener('click', () => {
         this.publisher.set(o.value);
+        this.close();
       });
+
       this.menu.appendChild(button);
     });
   }
+
+  close = () => {
+    Object.assign(this.button.style, {
+      borderBottomLeftRadius: '3px',
+      borderBottomRightRadius: '3px',
+    });
+
+    Object.assign(this.menuContainer.style, {
+      height: 0,
+      visibility: 'hidden',
+    });
+
+    document.removeEventListener('click', this.close);
+  };
 }
 
 export { DropDown };
